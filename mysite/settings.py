bb32d8840efd04e147a1cdba8294399397781408
Dias01 <1aydar.amrin@gmail.com>
2019-04-23 20:30:40 +0600

    I am dias

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gu0jpeefgk=+9(u)sl5aollsb5+g#51+(9mu0(j@yy^3j339kk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'dias01']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    
 ]
 
 MIDDLEWARE = [
